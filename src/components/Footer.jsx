1169bbd2850a20a8e32478ac17835f44d9a88c02
Ashish Bhati <ashbhati26@gmail.com>
2024-12-16 23:58:49 +0530

    update footer secton

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
     <div>
-        <div>
-            <p className='py-5 text-sm text-center text-white'>© 2024 Ashish. All rights reserved.</p>
-        </div>
+      <div>
+        <p className="py-5 text-sm text-center text-white">
+          © 2024 Ashish. All rights reserved.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
